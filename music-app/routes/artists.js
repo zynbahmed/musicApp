43778d45b51dea082db7aa1465abeccdf7a3ceab
zahraa <zahraaalihasan279@gmail.com>
2024-02-08 15:22:00 +0300

    edit artiest route

--- music-app/routes/artists.js
+++ music-app/routes/artists.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const artistsCtrl = require('../controllers/artists')
 
 router.get('/artists/new', artistsCtrl.new)
-router.post('/artists', artists.create)
+router.post('/artists', artistsCtrl.createArtist)
 router.post('/albums/:id/artists', artistsCtrl.addToArtist)
 
 module.exports = router
--- music-app/server.js
+++ music-app/server.js
@@ -25,9 +25,8 @@ app.use(methodOverride('_method'))
 
 app.use('/', indexRouter)
 app.use('/albums', albumsRouter)
-
 app.use('/', require('./routes/posts'))
-
+app.use('/',artistsRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
