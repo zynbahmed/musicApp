e7f6f7fd130897e3b4d093adf942745a1f3f12dc
zynbahmed <201901241@student.polytechnic.bh>
2024-02-09 14:06:52 +0300

    created the functions of the album controller (nothing in the body yet)

--- music-app/controllers/albums.js
+++ music-app/controllers/albums.js
@@ -1,1 +1,23 @@
+const Album = require('../models/album');
+const Artist = require('../models/artist');
 
+const index = async (req, res) => {
+}
+
+const show = async (req, res) => {
+}
+
+const newAlbum = (req, res) => {
+
+}
+
+const create = async (req, res) => {
+
+}
+
+module.exports = {
+    index,
+    show,
+    new: newAlbum,
+    create
+  };
