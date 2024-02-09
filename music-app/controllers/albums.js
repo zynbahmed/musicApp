59bd6e3d3e1cbdadf80b8866f73f2a01249be939
zynbahmed <201901241@student.polytechnic.bh>
2024-02-09 14:28:42 +0300

    created the add album functionality

--- music-app/controllers/albums.js
+++ music-app/controllers/albums.js
@@ -10,7 +10,7 @@ const show = async (req, res) => {
 }
 
 const newAlbum = (req, res) => {
-
+    res.render('albums/new', { title: 'Add Album' });
 }
 
 const create = async (req, res) => {
--- music-app/routes/index.js
+++ music-app/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/albums')
 });
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
