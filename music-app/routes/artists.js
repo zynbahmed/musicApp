a7ebbcf5e2beaad7be9ca9d233810c9b4dea848e
zynbahmed <201901241@student.polytechnic.bh>
2024-02-10 15:49:50 +0300

    fixed the artist functionality

--- music-app/routes/artists.js
+++ music-app/routes/artists.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const artistsCtrl = require('../controllers/artists')
 
-router.get('/artists/new', artistsCtrl.new)
+router.get('/artists/new', artistsCtrl.newArtist)
 router.post('/artists', artistsCtrl.createArtist)
 router.post('/albums/:id/artists', artistsCtrl.addToArtist)
 
