43c15f9edbc8dd16776f09520400b97804789e90
zahraa <zahraaalihasan279@gmail.com>
2024-02-10 11:01:01 +0300

    review model & ejs page to add review

--- music-app/controllers/reviews.js
+++ music-app/controllers/reviews.js
@@ -1,1 +1,14 @@
+const Review=require('../models/review')
+
+
+const creatReview = async (req, res) => {
+    try {
+
+    
+      //await Post.save()
+    } catch (error) {
+      console.log(error)
+    }
+    res.redirect('/posts')
+  }
 
