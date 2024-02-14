0f7739b29ec1910a845a88ee9942dbeee59f6ff1
Ahmedrali1 <ahmedrali97@gmail.com>
2024-02-14 22:53:13 +0300

    project done

--- music-app/routes/albums.js
+++ music-app/routes/albums.js
@@ -1,29 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 const albumsCtrl = require('../controllers/albums')
 
-// GET /albums
 router.get('/', albumsCtrl.index)
 
-// GET /albums/new
 router.get('/new', albumsCtrl.newAlbum)
 
-// GET /albums/:id (show functionality) MUST be below new route
 router.get('/:id', albumsCtrl.show)
 
-router.get('/artists/:artistId/albums', albumsCtrl.albumsByArtist);
+router.get('/artists/:artistId/albums', albumsCtrl.albumsByArtist)
 
-// POST /albums
-router.post('/', albumsCtrl.create);
+router.post('/', albumsCtrl.create)
 
-// GET /albums/:id/edit
-router.get('/:id/edit', albumsCtrl.editAlbum);
+router.get('/:id/edit', albumsCtrl.editAlbum)
 
-// PUT /albums/:id
-router.put('/:id', albumsCtrl.updateAlbum);
+router.put('/:id', albumsCtrl.updateAlbum)
 
-// DELETE /albums/:id
-router.delete('/:id', albumsCtrl.deleteAlbum);
+router.delete('/:id', albumsCtrl.deleteAlbum)
 
-
-module.exports = router;
+module.exports = router
